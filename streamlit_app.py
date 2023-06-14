47a4725969f1bf1768dc5bfb4f2ca023dcc93bf0
ghoshdebapratim1 <89624268+ghoshdebapratim1@users.noreply.github.com>
2023-06-14 14:35:39 +0000

    Added dataframe and printed first few rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 #import libraries
 import streamlit as st
 import pandas as pd
-#import matplotlib.pyplot as plt
-#import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 #import plotly.figure_factory as ff
 
 #look for more information here https://docs.streamlit.io/library/cheatsheet
@@ -23,8 +23,22 @@ st.text('Add your Thesis here')
 st.header('Dataset')
 
 #add your dataset (delete dataset this is an example)
-BostonHousing = pd.read_csv("BostonHousing.csv")
+df = pd.read_csv("cbb.csv")
 
+#showing dataset
+st.table(df.head())
+st.text('Showing dataset and writting about it here')
+
+
+#Adding images to make your streamlit look visually better!
+st.image('pro.png')
+st.text('You can add photos with descriptions')
+
+#Adding 3-6 Visualizations using photos collected and made from your graph
+#adding images
+#adding graphs by images
+st.image('pasted image 0.png')
+st.text('Discription about your graph and visualizations')
 
 #adding graphs by making plotly_Chart
 # Plot!
